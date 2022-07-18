2ab5c1107b8ffd083ac7c2688b7068b09d9445de
Danny <lannyducas@gmail.com>
2022-07-18 16:34:09 -0400

    commit tree e353f7c5d27e45624071fbdb0215a6e152a1323c

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Danny Lucas';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
